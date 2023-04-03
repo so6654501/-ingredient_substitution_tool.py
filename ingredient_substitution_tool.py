9fc53431bd023774ce2ca48efe4ecf55d73b46c3
so6654501 <129784401+so6654501@users.noreply.github.com>
2023-04-03 20:45:22 +0300

    Create  ingredient_substitution_tool.py

---
+++ ingredient_substitution_tool.py
@@ -0,0 +1,13 @@
+# Код программы Ingredient Substitution Tool
+
+recipe = {
+    "eggs": 2,
+    "flour": 250,
+    "sugar": 100,
+    "butter": 50,
+    "milk": 200
+}
+
+original_ingredient = input("What ingredient do you want to replace? ")
+replacement_ingredient = input("What ingredient do you want to use instead? ")
+replacement_amount = int(input(f"How much {replacement_ingredient} do you
